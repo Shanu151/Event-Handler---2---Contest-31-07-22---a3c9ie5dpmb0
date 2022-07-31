237ded80055c08248488604071322ccb962635f8
Shanu151 <77886773+Shanu151@users.noreply.github.com>
2022-07-31 19:04:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import {useRef} from 'react';
 import '../styles/App.css';
 const App = () => {
-
+  const ref= useRef(null);
   const handleClick = (event) =>{
     // use console.log
     console.log("Button id is:-", event.currentTarget.id);
@@ -10,8 +11,8 @@ const App = () => {
   // do not remove the two buttons or change their id
   return (
     <div id="main">
-      <button onClick={handleClick} id="button-a">Button A</button>
-      <button onClick={handleClick} id="button-b">Button B</button>
+      <button ref={ref} onClick={handleClick} id="button-a">Button A</button>
+      <button ref={ref} onClick={handleClick} id="button-b">Button B</button>
     </div>
   );
 };
